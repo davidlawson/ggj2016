9b4f81c5ac6d5bf209375d17edab63e70b80514f
David Lawson <tech.lawson@gmail.com>
2016-01-30 15:43:11 +1100

    Eye movement working

---
+++ Unity/Assets/Main Scene/Scripts/EyeMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeMovement : MonoBehaviour {
+
+	float multiplier = 0.00005f;
+	float maxDistance = 0.03f;
+	Transform eye;
+
+	// Use this for initialization
+	void Start() 
+	{
+		this.eye = this.transform.GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 mousePos = new Vector3(Input.mousePosition.x - Screen.width/2, Input.mousePosition.y - Screen.height/2, 0);
+		Vector3 movement = mousePos * multiplier;
+		if (movement.magnitude > maxDistance)
+		{
+			movement = movement.normalized * maxDistance;
+		}
+
+		this.eye.transform.localPosition = movement;
+	}
+}
