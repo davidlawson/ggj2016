80274ad3b3ea70a7a17744df02251d58d2f2de34
David Lawson <tech.lawson@gmail.com>
2016-01-31 10:02:57 +1100

    Begin screen

---
+++ Unity/Assets/Main Scene/Scripts/BeginController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class BeginController : MonoBehaviour {
+
+	public CanvasGroup beginScreen;
+
+	public void BeginGame()
+	{
+		//Sequence seq = DOTween.Sequence();
+		beginScreen.DOFade(0.0f, 3.0f);
+	}
+}
