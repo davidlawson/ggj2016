43866e92ef90c3e3eb47328fa52d030503097ff8
David Lawson <tech.lawson@gmail.com>
2016-01-31 09:55:24 +1100

    Adding credits feature

---
+++ Unity/Assets/CreditsController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Colorful;
+using DG.Tweening;
+
+public class CreditsController : MonoBehaviour 
+{
+	public CanvasGroup canvasGroup;
+	public GaussianBlur cameraBlur;
+
+	public void ShowCredits()
+	{
+		GameObject.FindWithTag("Player").GetComponent<PlayerController>().movementType = MovementType.None;
+
+		cameraBlur.Amount = 0;
+		cameraBlur.enabled = true;
+
+		DOTween.To(() => cameraBlur.Amount, x => cameraBlur.Amount = x, 1.0f, 3.0f);
+		canvasGroup.DOFade(1.0f, 3.0f);
+	}
+}
