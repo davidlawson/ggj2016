ad96510c51613e75dcdec2fd00d562acaaa01a61
David Lawson <tech.lawson@gmail.com>
2016-01-31 12:35:52 +1100

    Plant generator

---
+++ Unity/Assets/PlantGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantGenerator : MonoBehaviour {
+
+	public GameObject[] plantPrefabs;
+
+	public int number;
+	public float boxSize;
+
+	void Start () 
+	{
+		for (int i = 0; i < number; i++)
+		{
+			int plantIndex = Random.Range(0, plantPrefabs.Length);
+			GameObject plant = plantPrefabs[plantIndex];
+			GameObject newPlant = Instantiate<GameObject>(plant);
+			newPlant.transform.SetParent(transform, true);
+			newPlant.transform.position = new Vector3(
+				Random.Range(-boxSize, boxSize), 
+				0, 
+				Random.Range(-boxSize, boxSize)
+			);
+		}
+	}
+}
