2e3ab7f10d22814aae4fdcbca56f909b71b92551
David Lawson <tech.lawson@gmail.com>
2016-01-31 14:22:19 +1100

    David

--- Unity/Assets/GameState.cs
+++ Unity/Assets/GameState.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class GameState : MonoBehaviour 
 {
+	public Transform player;
+	public Transform a, b, c;
+	public SpriteRenderer playerSprite;
+	public SpriteRenderer ballSprite;
+
 	int _gemsCollected;
 	public int gemsCollected
 	{
@@ -16,5 +21,25 @@ public class GameState : MonoBehaviour
 			}
 		}
 		get { return _gemsCollected; }
+	}
+
+	void Update()
+	{
+		float dA = (a.position - player.position).magnitude;
+		float dB = (b.position - player.position).magnitude;
+		float dC = (c.position - player.position).magnitude;
+
+		if (dA > dB && dA > dC)
+		{
+			// A closest
+		}
+		else if (dB > dA && dB > dC)
+		{
+			// B closest
+		}
+		else if (dC > dA && dC > dB)
+		{
+			// C closest
+		}
 	}
 }
